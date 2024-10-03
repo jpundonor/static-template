f99d4817e77f975c714f36dd0dbed6d52ab9d01a
Javier Rojas <jardlml1209@gmail.com>
2024-10-03 18:31:45 -0300

    Update base path for static template in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/static-template/",
+  base: "https://jpundonor.github.io/static-template/",
 });
